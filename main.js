bb8b1001c6eb110e3e527cd7a5840daf2eebdc89
yoshifoxor <kurmelsa5d@gmail.com>
2020-01-30 07:40:37 +0900

    Implement slider

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // グローバル変数
 var tables = 6;
 var rowData = [], tt = [], route = [], station = [], desc = [];
-var Xhr = "";
+var Xhr = '';
 
 // ファイルの読込みと初期化
 function init(){
@@ -27,9 +27,9 @@ function init(){
         station[i] = tt[i][0][1];
         desc[i] = tt[i][0][2];
 
-        document.getElementById("route"+String(i+1)).innerHTML = route[i];
-        document.getElementById("station"+String(i+1)).innerHTML = station[i];
-        document.getElementById("desc"+String(i+1)).innerHTML = desc[i];
+        document.getElementById('route'+String(i+1)).innerHTML = route[i];
+        document.getElementById('station'+String(i+1)).innerHTML = station[i];
+        document.getElementById('desc'+String(i+1)).innerHTML = desc[i];
     }
 
     timetest();
@@ -38,27 +38,31 @@ function init(){
 // load continuous
 function timetest(){
     var i = 0;
-    var nt = "", duration = "";
+    var nt = '', duration = '';
     var now = new Date();
+    now.setHours(now.getHours() + (rangevalue / 60));
+    now.setMinutes(now.getMinutes() + (rangevalue % 60));
     //時刻調整用
     //now.setTime(now.getTime() + 2*24*60*60*1000 + 8*60*60*1000 + 17*60*1000);
 
-    document.getElementById("now").innerHTML = "現在時刻："+now.toLocaleString();
+    document.getElementById('now').innerHTML = '出発時刻：'+now.toLocaleString();
     for (i = 0; i < tables; i++){
         nt = nexttime(now,tt[i],true);
         if (nt === false){
             nt = nexttime(now,tt[i],false);
         }
         duration = (nt.time - now);
-        document.getElementById("next"+String(i+1)).innerHTML = nt.str[0]+"発 "+nt.str[3];
-        document.getElementById("dir"+String(i+1)).innerHTML = nt.str[2]+" "+nt.str[1];
+        document.getElementById('next'+String(i+1)).innerHTML = nt.str[0]+'発 '+nt.str[3];
+        document.getElementById('dir'+String(i+1)).innerHTML = nt.str[2]+' '+nt.str[1];
         //str[0]:時刻 str[1]:種別 str[2]:行先 str[3]:備考
         if (duration >= 0) {
-            document.getElementById("dur"+String(i+1)).innerHTML = "あと"+computeDuration(duration);
+            document.getElementById('dur'+String(i+1)).innerHTML = 'あと'+computeDuration(duration);
         }
     }
 
-    window.setTimeout("timetest()", 1000);
+    document.getElementById('sli').innerHTML = 'オフセット：'+rangevalue+'分';
+
+    window.setTimeout('timetest()', 1000);
 }
 
 // table中のnow以降の発車時刻を返す
@@ -66,7 +70,7 @@ function timetest(){
 // flagがtrueの場合は前日の24時以降の発車時刻を検索
 // 祝日判定にはjapanese.holiday.js(https://gist.github.com/Songmu/703311)を利用
 function nexttime(now, table, flag){
-    var i = 0, day = "";
+    var i = 0, day = '';
     var time = hhmm(now,flag);
     var next = new Date(now);
 
@@ -86,17 +90,17 @@ function nexttime(now, table, flag){
 
     for (i = 0; i < table.length; i++){
         if ( day > 0 && day < 6 ) {
-            if (table[i][0] === "[平日]"){
+            if (table[i][0] === '[平日]'){
                 break;
             }
         }
         else if ( day === 6 ) {
-            if (table[i][0] === "[土曜]"){
+            if (table[i][0] === '[土曜]'){
                 break;
             }
         }
         else if ( day === 0 ) {
-            if (table[i][0] === "[日曜・祝日]"){
+            if (table[i][0] === '[日曜・祝日]'){
                 break;
             }
         }
@@ -108,7 +112,7 @@ function nexttime(now, table, flag){
         }
     }
 
-    if (!table[i] || (table[i][0][0] === "[")){
+    if (!table[i] || (table[i][0][0] === '[')){
         if (flag){
             return false;
         }
@@ -120,8 +124,8 @@ function nexttime(now, table, flag){
     }
 
     // table[i][0] が24時を超えていた場合は翌日の日時に修正
-    var hh = "", mm = "";
-    if ( table[i][0].substring(0,2) > "23" ){
+    var hh = '', mm = '';
+    if ( table[i][0].substring(0,2) > '23' ){
         hh = parseInt(table[i][0].substring(0,2),10)-24;
         if (!flag) {
             next.setDate(next.getDate() + 1);
@@ -141,7 +145,7 @@ function nexttime(now, table, flag){
 function opencsv(src){
     var path = src;
     Xhr = new XMLHttpRequest();
-    Xhr.open("GET",path,false);
+    Xhr.open('GET',path,false);
     Xhr.send(null);
 
     return Xhr.responseText;
@@ -152,14 +156,14 @@ function computeDuration(ms){
     var h = String(Math.floor(ms / 3600000));
     var m = String(Math.floor((ms - h * 3600000)/60000)+ 100).substring(1);
     var s = String(Math.round((ms - h * 3600000 - m * 60000)/1000)+ 100).substring(1);
-    return h+":"+m+":"+s;
+    return h+':'+m+':'+s;
 }
 
 // 時刻を hh:mm 形式の文字列で返す
 // flagがtrueの場合は時刻を24進めた値を返す
 function hhmm(time,flag){
     if (flag) {
-        return String(time.getHours()+24)+":"+String(time.getMinutes()+100).substring(1);
+        return String(time.getHours()+24)+':'+String(time.getMinutes()+100).substring(1);
     }
-    return String(time.getHours()+100).substring(1)+":"+String(time.getMinutes()+100).substring(1);
+    return String(time.getHours()+100).substring(1)+':'+String(time.getMinutes()+100).substring(1);
 }
